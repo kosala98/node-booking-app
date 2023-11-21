a31af16ac9982bc45536b64fcb6c26ecc9f6ea2f
Kosala Dayarathne <dayarathnekosala90@gmail.com>
2023-11-22 00:31:41 +0530

    crud complete

---
+++ backend/controllers/bookingController.js
@@ -0,0 +1,86 @@
+const Booking = require("../models/BookingModel");
+const mongoose = require("mongoose");
+
+// GET all bookings
+const getBookings = async (req, res) => {
+  const bookings = await Booking.find({}).sort({ createdAt: -1 });
+
+  res.status(200).json(bookings);
+};
+
+// GET a single booking
+const getBooking = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Invalid booking" });
+  }
+
+  const booking = await Booking.findById(id);
+
+  if (!booking) {
+    return res.status(404).json({ error: "Booking not found" });
+  }
+
+  res.status(200).json(booking);
+};
+
+// CREATE new booking
+const createBooking = async (req, res) => {
+  const { title, load, reps } = req.body;
+
+  // Add document to DB
+  try {
+    const booking = await Booking.create({ title, load, reps });
+    res.status(200).json(booking);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// DELETE a booking
+const deleteBooking = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Invalid booking" });
+  }
+
+  const booking = await Booking.findOneAndDelete({ _id: id });
+
+  if (!booking) {
+    return res.status(400).json({ error: "Booking not found" });
+  }
+
+  res.status(200).json(booking);
+};
+
+// UPDATE a booking
+const updateBooking = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Invalid booking" });
+  }
+
+  const booking = await Booking.findOneAndUpdate(
+    { _id: id },
+    {
+      ...req.body,
+    }
+  );
+
+  if (!booking) {
+    return res.status(400).json({ error: "Booking not found" });
+  }
+
+  res.status(200).json(booking);
+};
+
+module.exports = {
+  getBookings,
+  getBooking,
+  createBooking,
+  deleteBooking,
+  updateBooking,
+};
--- backend/routes/bookings.js
+++ backend/routes/bookings.js
@@ -1,38 +1,26 @@
 const express = require("express");
-const Booking = require("../models/BookingModel");
-
+const {
+  createBooking,
+  getBookings,
+  getBooking,
+  deleteBooking,
+  updateBooking,
+} = require("../controllers/bookingController");
 const router = express.Router();
 
 // GET all bookings
-router.get("/", (req, res) => {
-  res.json({ msg: "Get all bookings" });
-});
+router.get("/", getBookings);
 
 // GET a single booking
-router.get("/:id", (req, res) => {
-  res.json({ msg: "GET a single booking" });
-});
+router.get("/:id", getBooking);
 
 // POST a new booking
-router.post("/", async (req, res) => {
-  const { title, load, reps } = req.body;
-
-  try {
-    const booking = await Booking.create({ title, load, reps });
-    res.status(200).json(booking);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post("/", createBooking);
 
 // DELETE a booking
-router.delete("/:id", (req, res) => {
-  res.json({ msg: "DELETE a booking" });
-});
+router.delete("/:id", deleteBooking);
 
 // UPDATE a booking
-router.patch("/:id", (req, res) => {
-  res.json({ msg: "UPDATE a booking" });
-});
+router.patch("/:id", updateBooking);
 
 module.exports = router;
