b0fd60b7da7bf7835d1d5ee67f7280a7c2a78be2
Kosala Dayarathne <dayarathnekosala90@gmail.com>
2023-11-30 13:15:08 +0530

    add user routes,controller & model

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const bookingRoutes = require("./routes/bookings");
+const userRoutes = require("./routes/user");
 
 // Declare exress app
 const app = express();
@@ -15,6 +16,7 @@ app.use((req, res, next) => {
 
 // Routes
 app.use("/api/bookings", bookingRoutes);
+app.use("/api/user", userRoutes);
 
 // Connect to DB
 mongoose
